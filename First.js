64426be65d7003b957ebf12b8bd76b9851282e68
beokku <ryan.groome@outlook.com>
2022-03-18 00:04:56 -0500

    Additions to Buttons

--- First.js
+++
@@ -1,8 +0,0 @@
-console.log("hey Welcome");
-
-const prompt = require('prompt-sync')();
-
-let fname = prompt("Enter your first name : ");
-let lname = prompt("Enter you last name :");
-
-console.log("Your full name is, ", fname,lname);
