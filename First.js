8d14e2aab9dfce3e42f5190d2d48a61fb891826d
beokku <ryan.groome@outlook.com>
2022-03-17 23:22:34 -0500

    Begin 2 (User Prompt)

--- First.js
+++ First.js
@@ -1,1 +1,8 @@
-console.log("hey");
+console.log("hey Welcome");
+
+const prompt = require('prompt-sync')();
+
+let fname = prompt("Enter your first name : ");
+let lname = prompt("Enter you last name :");
+
+console.log("Your full name is, ", fname,lname);
