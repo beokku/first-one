ca1ee401827f09066482a6171e198515781deaef
beokku <ryan.groome@outlook.com>
2022-03-18 00:05:36 -0500

    2nd

---
+++ First.js
@@ -0,0 +1,8 @@
+console.log("hey Welcome");
+
+const prompt = require('prompt-sync')();
+
+let fname = prompt("Enter your first name : ");
+let lname = prompt("Enter you last name :");
+
+console.log("Your full name is, ", fname,lname);
